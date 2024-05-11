40dc91303338d05cc0fb9f6fb2dae875be8d3162
JS <shinjooseop@gmil.com>
2024-05-11 16:00:35 +0900

    nineth commit

--- src/component/Ocean.jsx
+++ src/component/Ocean.jsx
@@ -6,29 +6,30 @@ import { Water } from 'three-stdlib'
 extend({ Water })
 
 function Ocean() {
-  const ref = useRef();
-  const gl = useThree((state) => state.gl);
-  const waterNormals = useLoader(THREE.TextureLoader, process.env.PUBLIC_URL + '/textures/waternormals.jpg');
-  const geom = useMemo(() => new THREE.PlaneGeometry(1000, 1000), []);
+  // const ref = useRef();
+  // const gl = useThree((state) => state.gl);
+  // const waterNormals = useLoader(THREE.TextureLoader, process.env.PUBLIC_URL + '/textures/waternormals.jpg');
+  // const geom = useMemo(() => new THREE.PlaneGeometry(1000, 1000), []);
 
-  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+  // waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
  
-  const config = useMemo(
-    () => ({
-      textureWidth: 512,
-      textureHeight: 512,
-      waterNormals,
-      sunDirection: new THREE.Vector3(0, 0, 0),
-      sunColor: 0xffffff,
-      waterColor: '#00f9ff',
-      distortionScale: 3.7,
-      fog: false,
-      format: gl.encoding
-    }),
-    [waterNormals]
-  );
-  useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta * .04));
-  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />
+  // const config = useMemo(
+  //   () => ({
+  //     textureWidth: 512,
+  //     textureHeight: 512,
+  //     waterNormals,
+  //     sunDirection: new THREE.Vector3(0, 0, 0),
+  //     sunColor: 0xffffff,
+  //     waterColor: '#00f9ff',
+  //     distortionScale: 3.7,
+  //     fog: false,
+  //     format: gl.encoding
+  //   }),
+  //   [waterNormals]
+  // );
+  // useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta * .04));
+  return <></>
+  // <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />
 }
 
 
